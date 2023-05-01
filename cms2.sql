35f1eb3e2e4f5fa03c535c56c6fa02589d9d7452
Reedtztoja <100943794+Reedtztoja@users.noreply.github.com>
2023-05-01 15:56:10 +0200

    Add files via upload

---
+++ cms2.sql
@@ -0,0 +1,145 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Maj 01, 2023 at 03:41 PM
+-- Wersja serwera: 10.4.28-MariaDB
+-- Wersja PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cms2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `post`
+--
+
+CREATE TABLE `post` (
+  `ID` int(11) NOT NULL,
+  `timestamp` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `filename` varchar(1024) NOT NULL,
+  `memeTitle` text NOT NULL,
+  `removed` tinyint(1) NOT NULL,
+  `userID` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `post`
+--
+
+INSERT INTO `post` (`ID`, `timestamp`, `filename`, `memeTitle`, `removed`, `userID`) VALUES
+(1, '2023-05-01 15:37:32', 'img/fdf8ba4465e44a195282b1b849c6f9f1b5b88315b0f97efd42ecd2f333d41f78.webp', '1234', 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `user`
+--
+
+CREATE TABLE `user` (
+  `ID` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`ID`, `email`, `password`) VALUES
+(1, 'edekzgredek1337@op.pl', '$argon2i$v=19$m=65536,t=4,p=1$bDZDYWRiRXlPRUlkbC44bA$T6MB0D7GWJf/J68HXsVKJL323xpExpR560xXhr7aLOA');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `vote`
+--
+
+CREATE TABLE `vote` (
+  `id` int(11) NOT NULL,
+  `post_ID` int(11) NOT NULL,
+  `score` int(11) NOT NULL,
+  `user_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `vote`
+--
+
+INSERT INTO `vote` (`id`, `post_ID`, `score`, `user_ID`) VALUES
+(1, 1, 1, 1);
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `post`
+--
+ALTER TABLE `post`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indeksy dla tabeli `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indeksy dla tabeli `vote`
+--
+ALTER TABLE `vote`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `vote_ibfk_1` (`post_ID`),
+  ADD KEY `vote_ibfk_2` (`user_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `post`
+--
+ALTER TABLE `post`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `vote`
+--
+ALTER TABLE `vote`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `vote`
+--
+ALTER TABLE `vote`
+  ADD CONSTRAINT `vote_ibfk_1` FOREIGN KEY (`post_ID`) REFERENCES `post` (`ID`),
+  ADD CONSTRAINT `vote_ibfk_2` FOREIGN KEY (`user_ID`) REFERENCES `user` (`ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
